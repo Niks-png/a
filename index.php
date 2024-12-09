54cd051b64af845ed8b48bc0e891565486bb017f
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-12-09 14:23:40 +0200

    skibidi

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+// for ($x=2; $x<=100; $x+=2){
+//     echo"$x <br>";
+// }
+// $y=1;
+
+// echo "whileee";
+
+// while ($y<=100){
+//     if($y%2==0){
+//         echo"$y";
+//     }
+//     $y=$y+1;
+// } -->
+
+
+// echo "2 uzdevums";
+// $faktorials = 1*2*3*4*5*6*7*8*9*10;
+// echo($faktorials);
+// echo"skolotaja uzde";
+// $ž =1;
+
+// for($x=1; $x<=10; $x=$x+1){
+// $ž= $ž*$x;
+// }
+// echo"$ž";
+
+// echo"3 uzdevums";
+// $x_1=1;
+// for($x=1; $x<=1000; $x=$x+0){
+// echo"$x <br>";
+// $xplus1=$x_1 +$x;
+// $x_1=$x;
+// $x=$xplus1;
+// }
+
+echo"4 uzdevums";
+ $x=0;
+ for($x=3; $x<=100; $x=$x+1){
+if(($x % 3== 0 || $x % 5==0) && $x % 15 !=0){
+    echo"$x<br>";
+}
+ }
+
+
+?>
