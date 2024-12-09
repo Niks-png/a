b70fb1c2d38472e002b8a594762c28bb0a995e8f
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-12-09 15:10:30 +0200

    new

--- index.php
+++ index.php
@@ -35,13 +35,37 @@
 // $x=$xplus1;
 // }
 
-echo"4 uzdevums";
- $x=0;
- for($x=3; $x<=100; $x=$x+1){
-if(($x % 3== 0 || $x % 5==0) && $x % 15 !=0){
-    echo"$x<br>";
-}
- }
+// echo"4 uzdevums";
+//  $x=0;
+//  for($x=3; $x<=100; $x=$x+1){
+// if(($x % 3== 0 || $x % 5==0) && $x % 15 !=0){
+//     echo"$x<br>";
+// }
+//  }
 
+// $latv_pilsetas=["Limbzi"=>4,
+//                 "cesis"=>18,
+//                 "riga"=> 8];
+//                 print_r($latv_pilsetas);
+//  $dzivnieki=["kakis"=>"meow",
+//                 "suns"=>"wooofff",             
+//                  "bear "=> "raghhhh",
+//                   "skibidi_toilet"=>"skibidi toilet"];          
+//  print_r (array_keys($dzivnieki));
+
+
+
+
+// $dataStringi=file_get_contents("https://jsonplaceholder.typicode.com/posts");
+// $posts= json_decode($dataStringi,true);
+// var_dump($posts[69][title]);
+
+
+$dataStringi=file_get_contents("https://jsonplaceholder.typicode.com/posts");
+$posts= json_decode($dataStringi);
+foreach($posts as $pos){
+    echo"$pos->title<br>";
+
+}
 
 ?>
